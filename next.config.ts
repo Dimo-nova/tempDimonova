5938353b9d1693dd9f73a263dc247c31c27b8e35
Pablo <lpz.pablo.16@gmail.com>
2026-06-30 10:42:10 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./lib/i18n.ts");
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname,
+    root: fileURLToPath(new URL(".", import.meta.url)),
   },
 };
 
